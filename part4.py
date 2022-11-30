ccfd9b83a32404ec93ec3009fe9e0e45755d2ec7
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-11-29 21:30:39 -0500

    intial comments

---
+++ part4.py
@@ -0,0 +1,13 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+
+
+# Write a Python function called mult_list() to multiply all the numbers in a list.
+
+
+# Write a Python function called rev_string() to reverse a string.
+
+
+# Write a Python function called num_within() to check whether a number falls in a given range.
+
+
+# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
