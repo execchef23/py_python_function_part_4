14e5d3630cc313dfcf176444b01b6517252074a2
Michael Manley <michaelmanley@Michaels-MBP.lan>
2022-11-29 21:47:19 -0500

    part 2 added

--- part4.py
+++ part4.py
@@ -1,8 +1,29 @@
 # Write a Python function called max_num()to find the maximum of three numbers.
 
+def max_num(a,b,c):
+    return max([a,b,c])
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
 
 # Write a Python function called mult_list() to multiply all the numbers in a list.
 
+def mult_list(list):
+    #if empty list, return 0
+    if len(list) == 0:
+        return 0
+    prod = list[0]
+
+    if len(list) > 1:
+        for i in list[1:]:
+            prod = prod * i
+
+        return prod
+
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
 
 # Write a Python function called rev_string() to reverse a string.
 
